53c695e3485e2f73a293c685da3000d83353bfc7
limpizzz <152085829+limpizzz@users.noreply.github.com>
2024-05-11 14:48:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+token = '6793082893:AAEzwEgPYk14p8E1sp6bsAouiTStf2NuA00'
+folder_id = 'b1gb1o17aj4e22s1c3m4'
+iam = 't1.9euelZqNmZTNi86KmsuWm8rGjIuXi-3rnpWajZmMmcqTisnKyJPOjYmVlpnl8_cpSwJO-e9lIThT_N3z92l5f03572UhOFP8zef1656VmpCUjIycxpKSm4uOzseQnM6W7_zF656VmpCUjIycxpKSm4uOzseQnM6WveuelZqYkMuNkJ3HipuSm8rLjIrHm7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.No8OT2NNw_gky-Z-Hs7j3ToJReSpIb_gskZJnpu1Jl3bmzDMtNEo1nUcnLDbiAvAfYVt3FAy-XHMQ-Je5lT6CA'
+admins_ids = ''
+
+maxusers = 3
+max_gpt_tokens = 120
+count_last_msg = 4
+tokenize_url = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
+gptmodel = 'yandexgpt-lite'
+url_gpt = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+max_user_stt_blocks = 10
+max_user_tts_symbols = 5_000
+max_user_gpt_tokens = 2_000
+
+LOGS = 'creds/logs.log' 
+DB_FILE = 'messages.db'
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Будь добрым другом пользователя'}]
